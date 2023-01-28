88b3aeb72729e460a791a112b947df3dc5885116
Kattesh <kate.sh@icloud.com>
2023-01-27 18:35:05 -0600

    hashrouter?

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import * as ReactDOMClient from "react-dom/client";
 import {Provider} from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -15,9 +15,9 @@ const root = ReactDOMClient.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
